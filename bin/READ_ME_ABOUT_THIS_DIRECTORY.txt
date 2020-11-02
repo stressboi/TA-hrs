f9843fe34c2d3a60796d6845f281963309ba1a54
James Brodsky <jbrodsky@splunk.com>
2020-11-02 16:18:26 -0700

    Initial commit

---
+++ bin/READ_ME_ABOUT_THIS_DIRECTORY.txt
@@ -0,0 +1,14 @@
+You will need to place the nmap distribution into the "nmap" directory.
+
+You can get it from: https://nmap.org/dist/nmap-7.91-win32.zip
+
+You MAY need to ensure the 2013 Microsoft Visual C++ Redistributable is installed.
+
+That's in the nmap distro, and is called "vcredist_x86.exe"
+
+IF you want to run the OS fingerprinting version of the scan, you need to install npcap, too.
+
+That's also in the nmap distro, and is called "npcap-1.00.exe"
+
+
+
---
+++ bin/scanrouter.bat
@@ -0,0 +1,53 @@
+@echo off
+REM -------------------------------------------
+REM brodsky@splunk.com 29OCT20
+REM figures out the local default gateway and nmap scans it for delivery of data via XML to Splunk
+REM delivers data into two files: $COMPUTERNAME_default_gw_output.log and $COMPUTERNAME_$DEFAULTGW.xml
+REM these files then read by monitor stanzas
+REM requires nmap CLI for windows - see the readme
+REM -------------------------------------------
+
+setlocal EnableDelayedExpansion
+
+REM find the default gateway
+@For /f "tokens=3" %%* in (
+    'route.exe print ^|findstr "\<0.0.0.0\>"'
+) Do @Set "DefaultGateway=%%*"
+
+REM find the mac address of the gateway
+@For /f "tokens=2" %%* in (
+    'arp.exe -A %DefaultGateway% ^|findstr "dynamic"'
+) Do @Set "MAC=%%*"
+
+REM find the DNS 
+@For /f "tokens=2 delims=:" %%f in (
+    'echo exit^|nslookup 2^>nul'
+) Do @Set "DNS=%%f" ) 
+set "DNS=%DNS: =%" 
+
+REM find the external IP address
+@For /f %%i in ('curl http://ipinfo.io/ip') do @Set "IP=%%i"
+
+REM find the external AS
+@For /f "tokens=*" %%i in ('curl http://ipinfo.io/org') do @Set "ORG=%%i"
+
+REM find the IP location
+@For /f %%i in ('curl http://ipinfo.io/loc') do @Set "LOC=%%i"
+
+REM find the Network Category
+@For /f "usebackq tokens=2 delims=:" %%f in (
+    `powershell Get-NetConnectionProfile ^| findstr NetworkCategory`
+) Do @Set "CATEGORY=%%f" ) 
+set "CATEGORY=%CATEGORY: =%" 
+
+echo %date% %time% external_ip="%IP%" default_gateway="%DefaultGateway%" DNS="%DNS%" gateway_mac=%MAC% reporting_host="%COMPUTERNAME%" org="%ORG%" loc="%LOC%" network_category=%CATEGORY% >> "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\monitor\%COMPUTERNAME%_default_gw_output.log"
+
+if exist "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" (
+	copy "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\monitor\%COMPUTERNAME%_%DefaultGateway%.xml"
+	)
+
+REM if a Private network then run an nmap scan
+if %CATEGORY%==Private (
+     "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\nmap\nmap.exe" -oX "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" -A %DefaultGateway%
+     ) else (
+     echo "Public network, no nmap scan will occur, exiting..."
