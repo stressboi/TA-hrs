9a46ddecf91bdf13da9f811dfa71c75196543387
James Brodsky <jbrodsky@splunk.com>
2020-11-02 17:15:42 -0700

    Added checks for network name.

--- bin/scanrouter_with_os_fingerprint.bat
+++ bin/scanrouter_with_os_fingerprint.bat
@@ -42,14 +42,21 @@ REM find the Network Category
 ) Do @Set "CATEGORY=%%f" ) 
 set "CATEGORY=%CATEGORY: =%"
 
-echo %date% %time% external_ip="%IP%" default_gateway="%DefaultGateway%" DNS="%DNS%" gateway_mac=%MAC% reporting_host="%COMPUTERNAME%" org="%ORG%" loc="%LOC%" network_category=%CATEGORY% >> "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\monitor\%COMPUTERNAME%_default_gw_output.log"
+REM find the Name
+@For /f "usebackq tokens=2 delims=:" %%f in (
+    `powershell Get-NetConnectionProfile ^| findstr Name`
+) Do @Set "NAME=%%f" ) 
+set "NAME=%NAME: =%" 
+
+echo %date% %time% external_ip="%IP%" default_gateway="%DefaultGateway%" DNS="%DNS%" gateway_mac=%MAC% reporting_host="%COMPUTERNAME%" org="%ORG%" loc="%LOC%" network_category="%CATEGORY%" name="%NAME%" >> "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\monitor\%COMPUTERNAME%_default_gw_output.log"
 
 if exist "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" (
 	copy "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\monitor\%COMPUTERNAME%_%DefaultGateway%.xml"
 	)
  
 REM if a Private network then run an nmap scan
-if %CATEGORY%==Private (
+REM MODIFY the below (Frothly_WFH) to match your deployed network WFH profile
+if %CATEGORY%==Private IF %NAME%==Frothly_WFH (
      "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\nmap\nmap.exe" -O -oX "%SPLUNK_HOME%\etc\apps\TA-hrs\bin\output\%COMPUTERNAME%_%DefaultGateway%.xml" -A %DefaultGateway%
      ) else (
      echo "Public network, no nmap scan will occur, exiting..."
